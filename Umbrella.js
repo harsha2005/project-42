4c7e8dc589ab78977b2cf849aa1423bbd87bc80d
harsha2005 <83114387+harsha2005@users.noreply.github.com>
2021-05-04 20:56:18 +0530

    Add files via upload

---
+++ Umbrella.js
@@ -0,0 +1,24 @@
+class Umbrella {
+    constructor(x,y){
+        var options = {
+            isStatic: true,
+        }
+        this.image = loadImage("Walking Frame/walking_1.png");
+        this.umbrella = Bodies.circle(x,y,50,options);
+        this.radius = 50;
+        World.add(world, this.umbrella)
+        this.bestManimage = loadImage("bat/Bestman-01.png");
+        
+    }
+
+    display(){
+        var pos = this.umbrella.position;
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y+70,300,300);  
+        
+        if(frameCount >= 200){
+            image(this.bestManimage,pos.x,pos.y+70,300,300);  
+        }
+        
+    }
+}
